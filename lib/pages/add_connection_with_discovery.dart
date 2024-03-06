d36edd529b88966a6b2f20d086b92aaeff8b2294
Damien Albisson <UF205DAL@net.elsys-design.com>
2024-03-06 17:11:40 +0100

    Add ui of the 3 differents way of adding a connection mqtt : manual, discovery and cloud

---
+++ lib/pages/add_connection_with_discovery.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:panduza_sandbox_flutter/pages/add_connection_form.dart';
+
+import '../data/const.dart';
+import 'home/mqtt_connection_form.dart';
+
+// Page with the 3 choices of adding connection :
+// with manual input, with discovery or with the cloud
+class ManualConnectionPage extends StatefulWidget {
+  
+  ManualConnectionPage({
+    super.key,
+    required this.ip,
+    required this.port,
+  });
+
+  String ip = "";
+  String port = "";
+
+  @override
+  _ManualConnectionPageState createState() => _ManualConnectionPageState();
+}
+
+class _ManualConnectionPageState extends State<ManualConnectionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // bar at the top of the application
+      appBar: AppBar(
+        // color of hamburger button
+        iconTheme: IconThemeData(color: white),
+        backgroundColor: black,
+        title: Text(
+          // widget.title,
+          "Add connection",
+          style: TextStyle(
+            color: blue,
+          ),
+        ),
+        // Panduza logo
+        // TO DO : Change to logo2 
+        actions: <Widget>[
+          IconButton(
+            icon: Image.asset('../../assets/icons/logo_1024.png'),
+            /*            
+            icon: SvgPicture.asset(
+              '../../assets/icons/logo2.svg'
+            ),
+            */
+            iconSize: 50,
+            onPressed: () {
+              return;
+            }, 
+          )
+        ],
+      ),
+      body: const Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[AddConnectionForm()],
+        ),
+      ),
+    );
+  }
+}
